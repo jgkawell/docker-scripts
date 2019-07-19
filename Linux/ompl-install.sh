bc0baa45c2806189193b83175d07ef1a680fb484
Jack Kawell <jgkawell@outlook.com>
2019-07-19 13:10:04 -0600

    Working version in simple form

--- Linux/ompl-install.sh
+++ Linux/ompl-install.sh
@@ -3,46 +3,8 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install git build-essential cmake tmux wget
 
-# Install libccd (https://github.com/danfis/libccd):
-sudo apt -y install m4
-cd ~/
-git clone https://github.com/danfis/libccd.git
-cd libccd
-git checkout tags/v2.1
-mkdir build
-cd build
-cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=ON ..
-make -j `nproc`
-sudo make install
-
-# Install OctoMap (https://github.com/OctoMap/octomap/wiki/Compilation-and-Installation-of-OctoMap):
-sudo apt -y install doxygen libqt4-dev libqt4-opengl-dev libqglviewer-dev-qt4
-cd ~/
-git clone https://github.com/OctoMap/octomap.git
-cd octomap
-git checkout tags/v1.9.0
-mkdir build
-cd build
-cmake ..
-make -j `nproc`
-make test
-sudo make install
-
-# Install FCL (https://github.com/flexible-collision-library/fcl):
-sudo apt -y install libeigen3-dev
-cd ~/
-git clone https://github.com/flexible-collision-library/fcl.git
-cd fcl
-git checkout tags/0.5.0
-sed -i "s|find_package(octomap QUIET)| |g" ./CMakeLists.txt
-mkdir build
-cd build
-cmake ..
-make -j `nproc`
-sudo make install
-
 # Install ROS (https://ros.org/install):
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt -y update
 sudo apt -y install ros-kinetic-desktop
@@ -61,11 +23,12 @@ wstool merge -t src https://raw.githubusercontent.com/ros-planning/moveit/master
 wstool update -t src
 rosdep -y install --from-paths src --ignore-src --rosdistro kinetic
 catkin config --extend /opt/ros/kinetic --cmake-args -DCMAKE_BUILD_TYPE=Release
-config --blacklist
-    moveit_chomp_optimizer_adapter
-    moveit_planners_chomp
+config --blacklist \
+    moveit_chomp_optimizer_adapter \
+    moveit_planners_chomp \
     chomp_motion_planner
 catkin build
+echo "source ~/ws_moveit/devel/setup.bash" >> ~/.bashrc
 
 # Install OMPL (http://ompl.kavrakilab.org/installation.html):
 sudo apt -y install pkg-config libboost-serialization-dev libboost-filesystem-dev libboost-system-dev libboost-program-options-dev libboost-test-dev libode-dev
@@ -87,3 +50,4 @@ wget https://raw.githubusercontent.com/ros-gbp/ompl-release/debian/kinetic/xenia
 cd ~/ws_moveit
 sudo catkin clean -y
 sudo catkin build # sudo is needed since we install OMPL in /opt
+source ~/.bashrc
