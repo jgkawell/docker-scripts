fe86439a89d18a0008d8eb3bef23a921b50245bc
Jack Kawell <jgkawell@outlook.com>
2019-12-03 21:47:50 -0700

    Removed logging in launch script

--- tools/linux/launch.sh
+++ tools/linux/launch.sh
@@ -17,7 +17,6 @@ HOST=$3
 # Add the host tag and setup XAuth if it's Nvidia
 if [[ "${HOST}" == "nvidia" ]]; then
     HOST_TAG='-nvidia'
-    echo "Setting up XAuth..."
     source setup_xauth.sh
 else
     HOST_TAG=''
@@ -35,14 +34,12 @@ if [[ "${REPO}" == "ompl" ]]; then
 fi
 
 # Pull down the image
-echo "Attempting to pull from: jgkawell/${REPO}:${TAG}${HOST_TAG}"
 docker pull jgkawell/${REPO}:${TAG}${HOST_TAG}
 
 # Add non-network local connections to control list
 xhost +local:root
 
 # Bring up the container
-echo "Using docker-compose file: ${REPO}/${SUB_DIR}${HOST}.docker-compose.yml"
 docker-compose -f ../../${REPO}/${SUB_DIR}${HOST}.docker-compose.yml up
 
 # Remove non-network local connections from control list
