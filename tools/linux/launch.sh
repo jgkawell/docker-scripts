ad5da3eeb79a684f5cd75307ded6c11100e2f351
Jack Kawell <jgkawell@outlook.com>
2019-12-03 21:42:46 -0700

    Working launch script without switch case

--- tools/linux/launch.sh
+++ tools/linux/launch.sh
@@ -1,47 +1,35 @@
+#!/bin/bash
 
-for i in "$@"
-do
-case $i in
-    -r|--repo)
-        REPO=${NEXT}
-        shift
-        ;;
-    -t|--tag)
-        NEXT=i+1
-        TAG=${NEXT}
-        shift
-        ;;
-    -h|--host)
-        NEXT=i+1
-        HOST=${NEXT}
-        shift
-        ;;
-    --help)
-        # unknown option -> show help
-        echo "Usage: bash launch.sh [options]"
-        echo "  -r|--repo: Docker repository name (x11/ros/ompl)"
-        echo "  -t|--tag: Docker repository tag prefix (base/sawyer/lfd)"
-        echo "  -h|--host: Docker host type (windows/intel/nvidia)"
-        exit
-    ;;
-esac
-done
+# Help documentation
+if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then
+    echo "Usage: bash launch.sh [options]"
+    echo "  \$1: Docker repository name (x11/ros/ompl)"
+    echo "  \$2: Docker repository tag prefix (base/sawyer/lfd)"
+    echo "  \$3: Docker host type (windows/intel/nvidia)"
+    exit
+fi
+
+# Set globals
+REPO=$1
+TAG=$2
+HOST=$3
 
 # Add the host tag and setup XAuth if it's Nvidia
-if [ ${HOST}=='nvidia' ]; then
+if [[ "${HOST}" == "nvidia" ]]; then
     HOST_TAG='-nvidia'
+    echo "Setting up XAuth..."
     source setup_xauth.sh
 else
     HOST_TAG=''
 fi
 
 # For the OMPL repository, mark the subdirectory
-if [ ${REPO}=='ompl' ]; then
-    if [ ${TAG}=='base' ]; then
+if [[ "${REPO}" == "ompl" ]]; then
+    if [[ "${TAG}" == "base" ]]; then
         SUB_DIR=''
-    elif [ ${TAG}=='sawyer' ]; then
+    elif [[ "${TAG}" == "sawyer" ]]; then
         SUB_DIR='sawyer/'
-    elif [ ${TAG}=='lfd' ]; then
+    elif [[ "${TAG}" == "lfd" ]]; then
         SUB_DIR='lfd/'
     fi
 fi
@@ -54,6 +42,7 @@ docker pull jgkawell/${REPO}:${TAG}${HOST_TAG}
 xhost +local:root
 
 # Bring up the container
+echo "Using docker-compose file: ${REPO}/${SUB_DIR}${HOST}.docker-compose.yml"
 docker-compose -f ../../${REPO}/${SUB_DIR}${HOST}.docker-compose.yml up
 
 # Remove non-network local connections from control list
