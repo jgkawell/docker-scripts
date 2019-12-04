3d23567acb3db350df119b2d06084841aa6cefd1
Jack Kawell <jgkawell@outlook.com>
2019-12-04 10:41:49 -0700

    Updated build script

--- build/build-images.sh
+++ build/build-images.sh
@@ -1,16 +1,20 @@
-export XAUTH=/tmp/.docker.xauth
-
-# Host can be either windows, intel (Linux), or nvidia (Linux)
-HOST=$1
-
-echo "STEP 1/4: Building ros:$HOST"
-docker-compose -f ../ros/$HOST.docker-compose.yml build
-
-echo "STEP 2/4: Build ompl:base-$HOST"
-docker-compose -f ../ompl/$HOST.docker-compose.yml build
-
-echo "STEP 3/4: Build ompl:sawyer-$HOST"
-docker-compose -f ../ompl/sawyer/$HOST.docker-compose.yml build
-
-echo "STEP 4/4: Build ompl:lfd-$HOST"
-docker-compose -f ../ompl/lfd/$HOST.docker-compose.yml build
+echo "---------- (1/5) X11 IMAGES ----------"
+docker pull jgkawell/x11:base
+docker build -f ./x11/Dockerfile --target base --cache-from jgkawell/x11:base -t jgkawell/x11:base ./x11
+docker build -f ./x11/Dockerfile --target nvidia --cache-from jgkawell/x11:base -t jgkawell/x11:base-nvidia ./x11
+echo "---------- (2/5) ROS IMAGES ----------"
+docker pull jgkawell/ros:base
+docker build -f ./ros/Dockerfile --target base --cache-from jgkawell/ros:base -t jgkawell/ros:base ./ros
+docker build -f ./ros/Dockerfile --target nvidia --cache-from jgkawell/ros:base -t jgkawell/ros:base-nvidia ./ros
+echo "---------- (3/5) OMPL BASE IMAGES ----------"
+docker pull jgkawell/ompl:base
+docker build -f ./ompl/Dockerfile --target base --cache-from jgkawell/ompl:base -t jgkawell/ompl:base ./ompl
+docker build -f ./ompl/Dockerfile --target nvidia --cache-from jgkawell/ompl:base -t jgkawell/ompl:base-nvidia ./ompl
+echo "---------- (4/5) OMPL SAWYER IMAGES ----------"
+docker pull jgkawell/ompl:sawyer
+docker build -f ./ompl/sawyer/Dockerfile --target base --cache-from jgkawell/ompl:sawyer -t jgkawell/ompl:sawyer ./ompl
+docker build -f ./ompl/sawyer/Dockerfile --target nvidia --cache-from jgkawell/ompl:sawyer -t jgkawell/ompl:sawyer-nvidia ./ompl
+echo "---------- (5/5) OMPL LFD IMAGES ----------"
+docker pull jgkawell/ompl:lfd
+docker build -f ./ompl/lfd/Dockerfile --target base --cache-from jgkawell/ompl:lfd -t jgkawell/ompl:lfd ./ompl
+docker build -f ./ompl/lfd/Dockerfile --target nvidia --cache-from jgkawell/ompl:lfd -t jgkawell/ompl:lfd-nvidia ./ompl
