f62aab6a685f7608ea8b24f705a577d182d730d4
Jack Kawell <jgkawell@outlook.com>
2019-12-03 19:28:17 -0700

    Updated old build script with new repo names

--- build/build-images.sh
+++ build/build-images.sh
@@ -3,14 +3,14 @@ export XAUTH=/tmp/.docker.xauth
 # Host can be either windows, intel (Linux), or nvidia (Linux)
 HOST=$1
 
-echo "STEP 1/4: Building ros-dev:$HOST"
+echo "STEP 1/4: Building ros:$HOST"
 docker-compose -f ../ros/$HOST.docker-compose.yml build
 
-echo "STEP 2/4: Build ompl-dev:base-$HOST"
+echo "STEP 2/4: Build ompl:base-$HOST"
 docker-compose -f ../ompl/$HOST.docker-compose.yml build
 
-echo "STEP 3/4: Build ompl-dev:sawyer-$HOST"
+echo "STEP 3/4: Build ompl:sawyer-$HOST"
 docker-compose -f ../ompl/sawyer/$HOST.docker-compose.yml build
 
-echo "STEP 4/4: Build ompl-dev:lfd-$HOST"
+echo "STEP 4/4: Build ompl:lfd-$HOST"
 docker-compose -f ../ompl/lfd/$HOST.docker-compose.yml build
