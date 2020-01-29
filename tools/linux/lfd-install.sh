0c936df201019bd50fa0e199984f3f348ab6a24a
Jack Kawell <jgkawell@outlook.com>
2020-01-28 17:01:06 -0700

    Updated local install scripts

--- tools/linux/lfd-install.sh
+++ tools/linux/lfd-install.sh
@@ -1,33 +1,44 @@
-# Clone CAIRO CC-LfD
-cd ~/ws_moveit/src && git clone https://github.com/cairo-robotics/cairo-lfd.git
+# Clone CAIRO LfD
+cd ~/catkin_ws/src && git clone https://github.com/cairo-robotics/cairo-lfd.git
+cd ~/catkin_ws/src/cairo-lfd && git checkout constraint_augmentation
+
+# Clone CAIRO constraint classification
+cd ~/catkin_ws/src && git clone https://github.com/cairo-robotics/constraint_classification.git
+
+# Clone CAIRO robot interface
+cd ~/catkin_ws/src && git clone https://github.com/cairo-robotics/cairo-robot-interface.git
+
+# Clone CAIRO collision / scene repo
+cd ~/catkin_ws/src && git clone https://github.com/cairo-robotics/collision_objects.git
+
+# Clone CAIRO feedback CC-LfD
+cd ~/catkin_ws/src && git clone https://github.com/jgkawell/feedback_cclfd.git
+
+# Clone CAIRO NLP
+cd ~/catkin_ws/src && git clone https://github.com/cairo-robotics/cairo-nlp.git
+
+# Install LfD Python requirements
+cd ~/catkin_ws/src/cairo-lfd && pip install -r requirements.txt
+cd ~/catkin_ws/src/cairo-nlp && pip install -r requirements.txt
+
+# Current workaround since Google Cloud installation is broken
+pip install --upgrade pip
+pip install --upgrade 'setuptools<45.0.0'
+pip install --upgrade 'cachetools<5.0'
+pip install --upgrade cryptography
+python -m easy_install --upgrade pyOpenSSL
 
 # Clone and install KDL for MoveIt plan listener
-cd ~/ws_moveit/src
+cd ~/catkin_ws/src
 git clone https://github.com/gt-ros-pkg/hrl-kdl.git
 cd hrl-kdl/pykdl_utils/
 python setup.py install
 cd ../hrl_geom
 python setup.py install
 
-# Clone CAIRO constraint classification
-cd ~/ws_moveit/src && git clone https://github.com/cairo-robotics/constraint_classification.git
-
-# Clone CAIRO feedback CC-LfD
-cd ~/ws_moveit/src && git clone https://github.com/jgkawell/cairo-feedback-cclfd.git
-
-# Clone CAIRO robot interface
-cd ~/ws_moveit/src && git clone https://github.com/cairo-robotics/cairo-robot-interface.git
-
-# Clone CAIRO collision / scene repo
-cd ~/ws_moveit/src && git clone https://github.com/cairo-robotics/collision_objects.git
-
-# Install LfD Python requirements
-cd ~/ws_moveit/src/cairo-lfd && pip install -r requirements.txt
-cd ~/ws_moveit/src/cairo-feedback-cclfd && pip install -r requirements.txt
-
 # Finally build the workspace
-cd ~/ws_moveit
+cd ~/catkin_ws
 sudo catkin build
 
 # Add Intera setup script alias
-echo 'alias sim="cd ~/ws_moveit && clear && ./intera.sh sim"' >> ~/.bashrc
+echo 'alias sim="cd ~/catkin_ws && clear && ./intera.sh sim"' >> ~/.bashrc
